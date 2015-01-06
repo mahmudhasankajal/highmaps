f524354340c9de34eecb89006023cee5509caed0
Mahmud Hasan <mahmud@oliverslearning.com>
2015-01-06 16:35:04 -0500

    Update Jan 6, 2015

--- report.js
+++ report.js
@@ -1,12 +1,34 @@
 /**
  * Created by Mahmud on 02/01/2015.
  */
-var my_data = [{"hc-key": "ca-ab","name":"Alberta","value": 1},{"hc-key": "ca-bc","name":"British Columbia","value": 2}, {"hc-key": "ca-mb","name":"Manitoba","value":3}, {"hc-key": "ca-nb","name":"New Brunswick","value": 4}, {"hc-key": "ca-nl","name":"Newfoundland and Labrador","value": 5}, {"hc-key": "ca-ns","name":"Nova Scotia","value": 6}, {"hc-key": "ca-nt","name":"Northwest Territories","value": 7}, {"hc-key": "ca-nu","name":"Nunavut","value": 8}, {"hc-key": "ca-on","name":"Ontario","value": 9}, {"hc-key": "ca-pe","name":"Prince Edward Island","value": 10}, {"hc-key": "ca-qc","name":"Quebec","value": 11}, {"hc-key": "ca-sk","name":"Saskatchewan","value": 12}, {"hc-key": "ca-yt","name":"Yukon","value": 13}];
+
+var current_map_level = 1;
+var min_map_level = 1;
+var max_map_level = 2;
+var clicked_point = null;
+
+var my_data = [{"hc-key": "ca-ab","name":"Alberta","value": 1,"id": 1},{"hc-key": "ca-bc","name":"British Columbia","value": 2,"id": 2}, {"hc-key": "ca-mb","name":"Manitoba","value":3,"id": 3}, {"hc-key": "ca-nb","name":"New Brunswick","value": 4,"id": 4}, {"hc-key": "ca-nl","name":"Newfoundland and Labrador","value": 5,"id": 5}, {"hc-key": "ca-ns","name":"Nova Scotia","value": 6,"id": 6}, {"hc-key": "ca-nt","name":"Northwest Territories","value": 7,"id": 7}, {"hc-key": "ca-nu","name":"Nunavut","value": 8,"id": 8}, {"hc-key": "ca-on","name":"Ontario","value": 9,"id": 9}, {"hc-key": "ca-pe","name":"Prince Edward Island","value": 10,"id": 10}, {"hc-key": "ca-qc","name":"Quebec","value": 11,"id": 11}, {"hc-key": "ca-sk","name":"Saskatchewan","value": 12,"id": 12}, {"hc-key": "ca-yt","name":"Yukon","value": 13,"id": 13}];
 
 var map_data = Highcharts.maps["countries/ca/ca-all"];
 
 function showKeys(obj){
     console.log(Object.keys(obj));
+}
+
+function getDataFromServer(){
+
+}
+
+function hidePopup(){
+    $("#point_menu_div").hide();
+}
+
+function createPopupMenu(){
+    $("#point_menu_div").prepend($("<a></a>").attr("href","#").text("View Detail").addClass("view_detail"));
+    if(current_map_level > min_map_level)
+        $("#point_menu_div").prepend($("<a></a>").attr("href","#").text("Go Up").addClass("go_up"));
+    if(current_map_level < max_map_level)
+        $("#point_menu_div").prepend($("<a></a>").attr("href","#").text("Drill Down").addClass("go_down"));
 }
 
 function loadMapData(map_data, data){
@@ -31,14 +53,12 @@ function loadMapData(map_data, data){
             }
         },
         chart: {
-            plotBackgroundColor: '#666666'
-            /*events: {
+            plotBackgroundColor: '#666666',
+            events: {
                 click: function (e) {
-                    var x = Math.round(e.xAxis[0].value),
-                        y = Math.round(e.yAxis[0].value);
-                    alert(x + ', ' + y);
+                    $("#point_menu_div").css("display","none");
                 }
-            }*/
+            }
         },
         series : [{
             data : data,
@@ -57,9 +77,9 @@ function loadMapData(map_data, data){
             point: {
                 events: {
                     click: function (e) {
-                        //alert(e.clientX + ', '+ e.clientY);
                         $("#point_menu_div").css("display","none");
                         $("#point_menu_div").css("display","block").css("left",e.clientX + "px").css("top",e.clientY+"px");
+                        clicked_point = this.id;
                         /*var text = '<b>Clicked point</b><br>Series: ' + this.series.name +
                                 '<br>Point: ' + this.name + ' (' + this.value + ')'
                             chart = this.series.chart;
@@ -83,8 +103,21 @@ function loadMapData(map_data, data){
 
 $(function(){
     $("#close_link").click(function(){
-        $("#point_menu_div").css("display","none");
+        hidePopup();
         return false;
     });
     loadMapData(map_data, my_data);
+    createPopupMenu();
+    $(".go_up").click(function(e){
+        hidePopup();
+        alert("id : " + clicked_point + " > Going up");
+    });
+    $(".go_down").click(function(e){
+        hidePopup();
+        alert("id : " + clicked_point + " > Drilling down");
+    });
+    $(".view_detail").click(function(e){
+        hidePopup();
+        alert("id : " + clicked_point + " > Loading detail");
+    });
 });
